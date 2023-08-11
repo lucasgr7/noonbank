2c16d0406cbbc2b110da3a7cd0792aa20270fb92
Lucas Garcia Ribeiro <lucasgr7@gmail.com>
2023-08-11 16:19:04 -0300

    feat(headerView): component created

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderView from './components/HeaderView.vue'
+
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <el-card>
+    <HeaderView :title="'Relatório de Gastos'"/>
+  </el-card>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+  .el-card{
+    border-radius: 5px;
+    background: rgba(244, 245, 240, 1);
+  }
+</style>s
---
+++ src/components/HeaderView.vue
@@ -0,0 +1,82 @@
+<script lang='ts' setup>
+import { ref } from 'vue';
+
+const props = defineProps(['title']);
+const filter = ref({} as any);
+
+const handleRefresh = () => {
+  console.log('refresh');
+}
+
+const handleClick = () => {
+  console.log('click');
+}
+
+const handleGoProfile = () => {
+  console.log('profile');
+}
+
+</script>
+
+<template>
+    <el-row class="container" align="middle">
+      <el-col :lg="14" :md="14" :sm="12" :xs="24">
+        <h2 class="title">{{ props.title }}</h2>
+      </el-col>
+      <el-col :lg="6" :md="5" :sm="5" :xs="24">
+        <el-date-picker v-model="filter" format="DD/MM/YYYY" type="day" placeholder="Data" style="width: 100%;"/>
+      </el-col>
+      <el-col :lg="4" :md="5" :sm="7" :xs="24">
+        <el-row class="icon_container"> 
+          <div class="circle" @click="handleRefresh()">
+            <img :src="'src/assets/icons/update-button.svg'" />
+          </div>
+          <div class="circle" @click="handleClick()">
+            <img :src="'src/assets/icons/plus-button.svg'" />
+          </div>
+          <div class="circle" @click="handleGoProfile()">
+            <img :src="'src/assets/icons/profile.png'" />
+          </div>
+        </el-row>
+      </el-col>
+    </el-row>
+</template>
+
+<style scoped lang='scss'>
+.container {
+  width: 100%;
+  padding: 0px 20px;
+
+  border-radius: 5px;
+}
+
+.title {
+  color: rgba(48, 48, 48, 1);
+  font-weight: 400;
+  font-size: 32px;
+}
+
+.circle {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 31px;
+  height: 31px;
+  background: rgba(255, 255, 255, 1);
+  border-radius: 50%;
+  margin-left: 25px;
+  cursor: pointer;
+}
+
+@media screen and (max-width: 767px) {
+
+  .container{
+    text-align: center;
+  }
+  .icon_container {
+    display: flex;
+    justify-content: space-evenly;
+    margin-top: 20px;
+  }
+}
+</style>
