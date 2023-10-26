8bc5d716a68cf293df8181b3805547ffee909b59
lucas.ribeiro <lucasgr7@gmail.com>
2023-10-26 09:28:58 -0300

    chore: formatation

--- src/components/handleChangePrimaryCateogry.ts
+++ src/components/handleChangePrimaryCateogry.ts
@@ -1,28 +1,27 @@
-import { TypeMergeData } from '../composables/useMergeTransaction';
-import { ElNotification } from 'element-plus';
+import { TypeMergeData } from "../composables/useMergeTransaction";
+import { ElNotification } from "element-plus";
 
 export function handleChangePrimaryCateogry(row: TypeMergeData) {
-// validate primaryCategory with notification
-if (!row.categoryId) {
-ElNotification({
-title: 'Error',
-message: 'Please select a category',
-type: 'error'
-});
-}
-try {
-if (row.type === 'credit') {
-updateCreditCardCategory(row);
-} else {
-updateAccountCategory(row);
-}
-}
-catch (error: unknown) {
-if (_.isError(error))
-ElNotification({
-title: 'Error',
-message: error.message,
-type: 'error'
-});
-}
+  // validate primaryCategory with notification
+  if (!row.categoryId) {
+    ElNotification({
+      title: "Error",
+      message: "Please select a category",
+      type: "error",
+    });
+  }
+  try {
+    if (row.type === "credit") {
+      updateCreditCardCategory(row);
+    } else {
+      updateAccountCategory(row);
+    }
+  } catch (error: unknown) {
+    if (_.isError(error))
+      ElNotification({
+        title: "Error",
+        message: error.message,
+        type: "error",
+      });
+  }
 }
