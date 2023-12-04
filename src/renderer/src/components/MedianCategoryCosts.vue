f64bcf66b8a3064fbadb4ac791e1f81c9cc3fc83
lucas.ribeiro <lucasgr7@gmail.com>
2023-12-04 16:04:53 -0300

    feat: median category fix

--- src/renderer/src/components/MedianCategoryCosts.vue
+++ src/renderer/src/components/MedianCategoryCosts.vue
@@ -24,7 +24,9 @@ import {
 
 import { TypeMergeData } from "../composables/useMergeTransaction";
 import { Category } from "../composables/useCategories";
+import useMedianCategoryCosts from "../composables/useMedianCategoryCosts";
 const mergeData = inject("mergeData") as Ref<TypeMergeData[]>;
+const categories = inject("categories") as Ref<Category[]>;
 
 use([
   CanvasRenderer,
@@ -36,14 +38,8 @@ use([
 ]);
 provide(THEME_KEY, "light");
 
-const props = defineProps({
-  categories: {
-    type: Array as () => Category[],
-    required: true,
-  },
-});
+const { render, option } = useMedianCategoryCosts(mergeData, categories);
 
-const option = ref();
 const visible = ref(false);
 
 function handleVisible() {
@@ -55,107 +51,6 @@ defineExpose({
   handleVisible,
 });
 
-function render() {
-  let dataByMonth: any = {};
-  let categories = new Set();
-  if (mergeData.value == null) {
-    console.error("mergeData is null");
-    return;
-  }
-
-  // Step 1: Aggregate data by month and category
-  mergeData.value.forEach((transaction: TypeMergeData) => {
-    const date = new Date(transaction.time);
-    const month = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(
-      2,
-      "0"
-    )}`;
-    const labelCategory = props.categories.find(
-      (category) => category.id === transaction.categoryId
-    )?.name?.toUpperCase();
-
-    if(labelCategory == null) return;
-
-    if (!dataByMonth[month]) dataByMonth[month] = {};
-    if (!dataByMonth[month][labelCategory ?? "none"])
-      dataByMonth[month][labelCategory ?? "none"] = 0;
-    dataByMonth[month][labelCategory ?? "none"] += transaction.amount;
-    categories.add(labelCategory ?? "none");
-  });
-
-  // Step 2: Prepare chart series data
-  let seriesData = Array.from(categories).map((category) => ({
-    name: category,
-    type: "bar",
-    data: [],
-  }));
-
-  let trendLine = {
-    name: "Trend",
-    type: "line",
-    data: [],
-  };
-
-  let xAxisData: any = [];
-
-  // Limit to last 5 months
-  let lastMonths = Object.keys(dataByMonth).sort();
-  lastMonths.forEach((month) => {
-    xAxisData.push(month);
-
-    seriesData.forEach((series) => {
-      series.data.push(dataByMonth[month][series.name] || 0);
-    });
-  });
-
-  // Step 3: Trend line calculation (simplified)
-  seriesData.forEach((series) => {
-    let total = series.data.reduce((acc, val) => acc + val, 0);
-    let avg = total / series.data.length;
-    trendLine.data.push(avg);
-  });
-
-  // Step 4: Update ECharts option
-  option.value = {
-    tooltip: {
-      /* your tooltip config */
-    },
-    color: [
-  "#B37676", "#B38E64", "#B3A564", "#86B382",
-  "#FFD9D6", "#D9E6FF", "#D4FFD4", "#FFE6B3",
-  "#B39C64", "#8686B3", "#B37699", "#7D9CB3",
-  "#FFF0B3", "#E0E0FF", "#FFD6E8", "#E6F2FF"
-    ],
-    toolbox: {
-      /* your toolbox config */
-    },
-    grid: {
-        top: '22%',
-        left: '1%',
-        right: '10%',
-        containLabel: true
-      },
-    legend: {
-      data: [...categories, "Trend"],
-    },
-    xAxis: [
-      {
-        type: "category",
-        data: xAxisData,
-        axisPointer: {
-          type: "shadow",
-        },
-      },
-    ],
-    yAxis: [
-      {
-        type: "value",
-        name: "Spending",
-      },
-    ],
-    series: [...seriesData, trendLine],
-  };
-}
 function handleClose() {
   visible.value = false;
   render();
@@ -169,6 +64,16 @@ function handleClose() {
     width="90%"
     height="70%"
   >
+    <el-row>
+      <el-toggle
+        v-model="visible"
+        active-color="#13ce66"
+        inactive-color="#ff4949"
+        active-text="Show"
+        inactive-text="Hide"
+        @change="handleVisible"
+      />
+    </el-row>
     <v-chart class="chart" @closed="handleClose" :option="option" autoresize />
   </el-dialog>
 </template>
@@ -180,4 +85,6 @@ function handleClose() {
   width: 100% auto !important;
   background: white;
   backdrop-filter: blur(10px);
-} </style>
+}
+</style>
+
